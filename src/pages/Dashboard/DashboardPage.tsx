6ad62176c76497c4bbbf60c50917841034a7ca73
kaamrepo <kaamappofficial@gmail.com>
2024-05-30 11:59:56 +0530

    updated file

--- src/pages/Dashboard/DashboardPage.tsx
+++ src/pages/Dashboard/DashboardPage.tsx
@@ -5,7 +5,6 @@ import ChartThree from '../../components/Charts/ChartThree';
 // import ChartTwo from '../../components/Charts/ChartTwo';
 import ChatCard from '../../components/Chat/ChatCard';
 import MapOne from '../../components/Maps/MapOne';
-import TableOne from '../../components/Tables/TableOne';
 import DefaultLayout from '../../layout/DefaultLayout';
 
 const DashboardPage = () => {
